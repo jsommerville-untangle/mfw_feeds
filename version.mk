2031660b1a9067260e36dcbe58403b23b4f58800
John Sommerville <jsommerville@untangle.com>
2020-12-11 10:51:35 -0700

    add some logic to accept other pkg branches if we find the current package name passed in

--- version.mk
+++ version.mk
@@ -1,7 +1,12 @@
 MFW_VERSION ?= $(shell ../build/git-remote-find-tag.sh $(1))
 MFW_COMMIT = $(shell ../build/git-remote-describe.sh $(1) $(2))
 
-PKG_SOURCE_VERSION := $(call MFW_VERSION,$(PKG_SOURCE_URL))
+ifneq (,$(filter $(PKG_NAME),$(MFW_OTHER_PKG_NAMES)))
+	PKG_SOURCE_VERSION := $(MFW_OTHER_PKG_BRANCH)
+else
+	PKG_SOURCE_VERSION := $(call MFW_VERSION,$(PKG_SOURCE_URL))
+endif
+
 PKG_VERSION := $(call MFW_COMMIT,$(PKG_SOURCE_URL),$(PKG_SOURCE_VERSION))
 
 PKG_SOURCE_SUBDIR := $(PKG_NAME)-$(PKG_VERSION)
